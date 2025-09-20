17e38d4b2a9b6c882c1dd435bfbb0dec0bb0b656
ThatOneStas <138216528+ThatOneStas@users.noreply.github.com>
2025-09-20 16:39:59 +0300

    Creating folder and files.

--- chain/contracts/StoreLogic.sol
+++ chain/contracts/StoreLogic.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-contract StoreLogic {
+contract Store {
     // лишаєм івент?
     event ItemBought(uint256 id, address buyer, uint256 price);
 
